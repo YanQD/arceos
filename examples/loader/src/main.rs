2db985634c6f0430358871540d54b229c137c56f
YanLien <YJQ980314@outlook.com>
2024-12-03 15:23:58 +0800

    exec2

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -6,22 +6,18 @@ use axstd::println;
 
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
 
-// struct App {
-//     size: usize,
-//     data: Vec<u8>
-// }
-
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = unsafe {*(apps_start)} as usize;
-
-    let apps_start = unsafe { apps_start.add(1) };
+    let mut apps_start = PLASH_START as *const u8;
 
     println!("Load payload ...");
 
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-    println!("content: {:?}: ", code);
+    while unsafe {*(apps_start)} as usize > 0 {
+        println!("App_Size: {}", unsafe{*apps_start});
+        let code = unsafe { core::slice::from_raw_parts(apps_start.add(1), *(apps_start) as usize) };
+        apps_start = unsafe { apps_start.add(*(apps_start) as usize + 1) };
+        println!("content: {:?}: ", code);
+    }
 
     println!("Load payload ok!");
 }
