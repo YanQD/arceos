69cdd6f3272d802878eb62279f17c4017a74d943
YanLien <YJQ980314@outlook.com>
2024-12-03 22:07:51 +0800

    base3

--- examples/loader/src/main.rs
+++ examples/loader/src/main.rs
@@ -5,7 +5,29 @@
 #[cfg(feature = "axstd")]
 use axstd::println;
 
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+
+fn register_abi(num: usize, handle: usize) {
+    unsafe { ABI_TABLE[num] = handle; }
+}
+
+fn abi_hello() {
+    println!("[ABI:Hello] Hello, Apps!");
+}
+
+fn abi_putchar(c: char) {
+    println!("[ABI:Print] {c}");
+}
+
 const PLASH_START: usize = 0xffff_ffc0_2200_0000;
+
+// app running aspace
+// SBI(0x80000000) -> App <- Kernel(0x80200000)
+// va_pa_offset: 0xffff_ffc0_0000_0000
+const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
@@ -17,11 +39,6 @@ fn main() {
     let load_code = unsafe { core::slice::from_raw_parts(load_start, load_size) };
     println!("load code {:?}; address [{:?}]", load_code, load_code.as_ptr());
 
-    // app running aspace
-    // SBI(0x80000000) -> App <- Kernel(0x80200000)
-    // va_pa_offset: 0xffff_ffc0_0000_0000
-    const RUN_START: usize = 0xffff_ffc0_8010_0000;
-
     let run_code = unsafe {
         core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
     };
@@ -31,14 +48,27 @@ fn main() {
 
     println!("Load payload ok!");
 
-    println!("Execute app ...");
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
 
-    unsafe {
-        core::arch::asm!("
-            li      t2, {run_start}
-            jalr    t2
-            j       .",
-            run_start = const RUN_START,
-        )
-    }
+	println!("Execute app ...");
+    let arg0: u8 = b'A';
+
+    // execute app
+    unsafe { core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        la      t1, {abi_table}
+        add     t1, t1, t0
+        ld      t1, (t1)
+        jalr    t1
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+        run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+        //abi_num = const SYS_HELLO,
+        abi_num = const SYS_PUTCHAR,
+        in("a0") arg0,
+    )}
 }
